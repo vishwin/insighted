060b99838efaab241940985d67e4bdaa2ddcab30
Charlie Li <git@vishwin.info>
2015-10-11 07:30:36 -0400

    Piece of shit code
    I'm pretty sure this leaks mad memory with repeated switching

--- ultraping.py
+++ ultraping.py
@@ -1,37 +1,54 @@
 import time
 import RPi.GPIO as GPIO
+import csv
+import boto3
+from boto3.sessions import Session
 
 GPIO.setmode(GPIO.BOARD)
 
+switchPin=7
+GPIO.setup(switchPin, GPIO.IN)
+pingPin=11
 timeout=0.020
 
-while 1:
-	GPIO.setup(11, GPIO.OUT)
-	GPIO.output(11, 0)
-	
-	time.sleep(0.000002)
-	GPIO.output(11, 1)
-	
-	time.sleep(0.000005)
-	GPIO.output(11, 0)
-	
-	GPIO.setup(11, GPIO.IN)
-	
-	goodread=True
-	watchtime=time.time()
-	while GPIO.input(11)==0 and goodread:
-		starttime=time.time()
-		if (starttime-watchtime > timeout):
-			goodread=False
-	
-	if goodread:
+def idle():
+	while GPIO.input(switchPin)==1:
+		pass
+	time.sleep(1)
+	return getPoints()
+
+def getPoints():
+	while GPIO.input(switchPin)==1:
+		GPIO.setup(pingPin, GPIO.OUT)
+		GPIO.output(pingPin, 0)
+		
+		time.sleep(0.000002)
+		GPIO.output(pingPin, 1)
+		
+		time.sleep(0.000005)
+		GPIO.output(pingPin, 0)
+		
+		GPIO.setup(pingPin, GPIO.IN)
+		
+		goodread=True
 		watchtime=time.time()
-		while GPIO.input(11)==1 and goodread:
-			endtime=time.time()
-			if (endtime-watchtime > timeout):
+		while GPIO.input(pingPin)==0 and goodread:
+			starttime=time.time()
+			if (starttime-watchtime > timeout):
 				goodread=False
-	
-	if goodread:
-		duration=endtime-starttime
-		distance=duration*34000/2
-		print(distance)
+		
+		if goodread:
+			watchtime=time.time()
+			while GPIO.input(pingPin)==1 and goodread:
+				endtime=time.time()
+				if (endtime-watchtime > timeout):
+					goodread=False
+		
+		if goodread:
+			duration=endtime-starttime
+			distance=duration*34320/2 # Duration accounts for round trip
+			print(distance)
+	time.sleep(1)
+	return idle()
+
+idle()
